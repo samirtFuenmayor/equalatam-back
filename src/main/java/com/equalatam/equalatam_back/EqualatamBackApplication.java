779158126d367a220593df4bc55b29d3f434558a
samirt <sfuenmayor@ecuasanitas.com>
2026-01-15 01:05:06 -0500

    Initial Spring Boot backend

---
+++ src/main/java/com/equalatam/equalatam_back/EqualatamBackApplication.java
@@ -0,0 +1,13 @@
+package com.equalatam.equalatam_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EqualatamBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EqualatamBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/equalatam/equalatam_back/EqualatamBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equalatam.equalatam_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EqualatamBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
